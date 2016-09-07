1af88a743486f17fc56ecee740557427427294a6
Misael Gaytan <gaytanmisael@gmail.com>
2016-09-07 12:02:34 -0700

    Added Facebook Login
    Added Facebook Login but no redirect

--- app/app.js
+++ app/app.js
@@ -7,6 +7,25 @@
   };
   firebase.initializeApp(config);
 
+
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '1280699998606760',
+      xfbml      : true,
+      version    : 'v2.6'
+    });
+
+    FB.Event.subscribe('auth.authResponseChange', checkLoginState);
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
 'use strict';
 
 // Declare app level module which depends on views, and components
